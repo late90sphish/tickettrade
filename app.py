8ee383e90b7ce5086d0df724eb0e8abcb72d8a12
late90sphish <pokehollertcg@gmail.com>
2026-08-19 19:01:32 -0400

    Production prep: env-controlled debug mode, add gunicorn

--- app.py
+++ app.py
@@ -997,4 +997,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
+    app.run(debug=debug_mode, port=5000)
